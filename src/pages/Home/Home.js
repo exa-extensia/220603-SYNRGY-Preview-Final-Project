8259b1a79971637c95383522831f0b2d3da6ca6f
exa-extensia <exa.extensia@gmail.com>
2022-06-02 17:06:51 +0700

    [xa] feat: setup Tailwind Config

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,34 @@
+export default function Jumbotron() {
+	return (
+		<div className="jumbo__bg">
+			<div className="jumbo__wrapper">
+				<div className="jumbo__left">
+					<h2 className="jumbo__title">Kenapa harus beli di Flambo ?</h2>
+					<p className="jumbo__desc">
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+						quae ab illo inventore veritatis et quasi architecto beatae vitae
+						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+						aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+						eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+						fugiat quo voluptas nulla pariatur?
+					</p>
+					<div className="warnawrapper">
+						<div className="testwarna warna1"></div>
+						<div className="testwarna warna2"></div>
+						<div className="testwarna warna3"></div>
+						<div className="testwarna warna4"></div>
+						<div className="testwarna warna5"></div>
+						<div className="testwarna warna6"></div>
+						<div className="testwarna warna7"></div>
+						<div className="testwarna warna8"></div>
+						<div className="testwarna warna9"></div>
+						<div className="testwarna warna10"></div>
+						<div className="testwarna warna11"></div>
+						<div className="testwarna warna12"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		colors: {
+			brown: "#A67A4A",
+			goldie: "#FFEDCD",
+			cream: "#FCF5EA",
+			black: "#515151",
+			grey: "#C4C4C4",
+			"med-gray": "#E3E3E3",
+			"soft-gray": "#F6F6F6",
+			success: "#7EC722",
+			warning: "#FFC634",
+			danger: "#FF6523",
+			primary: "#5FADF4",
+			neutral: "#C4C4C4",
+		},
+		fontFamily: {
+			sans: ["Nunito", "sans-serif"],
+		},
 		extend: {
 			container: {
 				center: true,
@@ -12,8 +29,9 @@ module.exports = {
 					"2xl": "8rem",
 				},
 			},
-			extend: {
-				fontFamily: {},
+			backgroundImage: {
+				"linear-cream":
+					"linear-gradient(180deg, #FCF5EA 0%, rgba(252, 245, 234, 0) 100%);",
 			},
 		},
 	},
