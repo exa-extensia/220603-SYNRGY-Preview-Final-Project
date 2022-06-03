888f0d6fbc773355e6e663140344d8b45d57267c
exa-extensia <exa.extensia@gmail.com>
2022-06-03 11:39:55 +0700

    [xa] feat: add button placeholder files, update container setup in TwConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,13 +21,21 @@ module.exports = {
 		extend: {
 			container: {
 				center: true,
-				padding: {
-					DEFAULT: "1rem",
-					sm: "2rem",
-					lg: "4rem",
-					xl: "6rem",
-					"2xl": "8rem",
+				mx: {
+					pp: "20px",
+					pl: "20px",
+					tp: "40px",
+					tl: "40px",
+					lp: "120px",
+					dp: "120px",
 				},
+				// padding: {
+				// 	DEFAULT: "1rem",
+				// 	sm: "2rem",
+				// 	lg: "4rem",
+				// 	xl: "6rem",
+				// 	"2xl": "8rem",
+				// },
 			},
 			backgroundImage: {
 				"linear-cream":
