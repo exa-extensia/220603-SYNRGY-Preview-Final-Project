0205efba94054d00e56e2841a5c02741651ae3c5
M Afwan Al Fiansyah <mafwanalfiansyah73@gmail.com>
2022-06-05 11:34:47 +0800

    [af] fix: responsive done (section3)

--- src/components/sections/home-section3/Home-section3.js
+++ src/components/sections/home-section3/Home-section3.js
@@ -14,25 +14,25 @@ import img12 from '../../../assets/images/logos/homepage-section3/hp-s3-12.png'
 export default function HomeSection3() {
     return (
         <section>
-            <div className="max-w-7xl my-10 container bg-cream max-w-screen-2lg" id="ribuan-brand">
-                <h1 className="text-center pt-20 text-2xl">Ribuan <span className="text-brown">BRAND</span> produk untuk
+            <div className="max-w-7xl my-10 container bg-cream max-w-screen-2lg lg:container" id="ribuan-brand">
+                <h1 className="text-center pt-10 text-sm sm:text-xl md:text-2xl">Ribuan <span className="text-brown">BRAND</span> produk untuk
                     menjadikanmu cantik
                 </h1>
                 <div className="rb__logoWrapper">
-                    <div className="grid grid-cols-5 gap-6 py-16">
-                        <img src={img1} className="m-auto" alt="img1" />
-                        <img src={img2} className="m-auto" alt="img2" />
-                        <img src={img3} className="m-auto" alt="img3" />
-                        <img src={img4} className="m-auto" alt="img4" />
-                        <img src={img5} className="m-auto" alt="img5" />
-                        <img src={img6} className="m-auto" alt="img6" />
-                        <img src={img7} className="m-auto" alt="img7" />
-                        <img src={img8} className="m-auto" alt="img8" />
-                        <img src={img9} className="m-auto" alt="img9" />
-                        <img src={img10} className="m-auto" alt="img10" />
-                        <img src={img11} className="m-auto" alt="img11" />
-                        <img src={img12} className="m-auto" alt="img12" />
-                        <a href="#" className="text-brown underline pt-5">Dan banyak lainnya</a>
+                    <div className=" grid grid-cols-3 gap-2 py-10  mx-2 md:grid-cols-5">
+                        <img src={img1} className="m-auto  pb-2" alt="img1" />
+                        <img src={img2} className="m-auto  pb-2" alt="img2" />
+                        <img src={img3} className="m-auto  pb-2" alt="img3" />
+                        <img src={img4} className="m-auto  pb-2" alt="img4" />
+                        <img src={img5} className="m-auto  pb-2" alt="img5" />
+                        <img src={img6} className="m-auto  pb-2" alt="img6" />
+                        <img src={img7} className="m-auto  pb-2" alt="img7" />
+                        <img src={img8} className="m-auto  pb-2" alt="img8" />
+                        <img src={img9} className="m-auto  pb-2" alt="img9" />
+                        <img src={img10} className="m-auto  pb-2" alt="img10" />
+                        <img src={img11} className="m-auto  pb-2" alt="img11" />
+                        <img src={img12} className="m-auto  pb-2" alt="img12" />
+                        <a href="#" className="text-brown underline text-xs w-[150%] sm:text-base sm:pl-7 md:pt-6 md:text-xl">Dan banyak lainnya</a>
                     </div>
                 </div>
             </div>
