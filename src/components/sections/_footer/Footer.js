2735dc556b596b69a3ff7ca987d39a3662fcb15e
exa-extensia <exa.extensia@gmail.com>
2022-07-07 16:53:59 +0700

    [xa] fix: fixing slicing Homepage

--- src/components/sections/_footer/Footer.js
+++ src/components/sections/_footer/Footer.js
@@ -1,42 +1,135 @@
-import BCA from '../../../assets/icons/icon-bank/BCA.svg'
-import mandiri from '../../../assets/icons/icon-bank/mandiri.svg'
-import bni from '../../../assets/icons/icon-bank/bni.svg'
-import jne from '../../../assets/icons/icon-pengiriman/jne.svg'
-import pos from '../../../assets/icons/icon-pengiriman/pos.svg'
+import bca from "../../../assets/icons/icon-bank/bca.png";
+import bni from "../../../assets/icons/icon-bank/bni.png";
+import bri from "../../../assets/icons/icon-bank/bri.png";
+import permata from "../../../assets/icons/icon-bank/permata.png";
+
+import jne from "../../../assets/icons/icon-pengiriman/jne.png";
+import pos from "../../../assets/icons/icon-pengiriman/pos.png";
+import tiki from "../../../assets/icons/icon-pengiriman/tiki.png";
+
+import illst from "../../../assets/images/footer-illst.png";
+import google from "../../../assets/images/footer-google.png";
+
+import { TbMail, TbPhone, TbBrandWhatsapp } from "react-icons/tb";
 
 export default function Footer() {
-    return (
-        <section className="footer bg-goldie">
-            <div className="footer-wrap grid grid-cols-2 p-5 gap-5 max-w-[600px] container sm:grid-cols-4 sm:max-w-2xl md:max-w-4xl lg:max-w-7xl">
-                <div className="footer__menu ">
-                    <h3 className="text-sm mb-2">MENU</h3>
-                    <a href="#" className="block">Home</a>
-                    <a href="#" className="block">Kategori</a>
-                    <a href="#" className="block">brand</a>
-                    <a href="#" className="block">Pria</a>
-                    <a href="#" className="block">Produk Organik</a>
-                    <a href="#" className="block">Wanita</a>
+	return (
+		<>
+			<section className="flex w-full items-center justify-center bg-goldie">
+				<div className="container mx-4 flex flex-col sm:max-w-[688px] sm:flex-row sm:gap-8 lg:max-w-[864px] xl:max-w-[1200px]">
+					<div className="grid grid-cols-2 gap-10 py-10  sm:w-3/5 sm:grid-cols-3 sm:gap-2 ">
+						<div className="flex  flex-col gap-4 sm:row-span-2 ">
+							<p className="text-right text-base font-bold sm:text-left sm:text-sm lg:text-lg">
+								MENU
+							</p>
+							<div className=" flex  flex-col place-items-end gap-2 sm:place-items-start sm:gap-1">
+								<a href="#" className="text-xs lg:text-base">
+									Home
+								</a>
+								<a href="#" className="text-xs lg:text-base">
+									Kategori
+								</a>
+								<a href="#" className="text-xs lg:text-base">
+									Produk Trending
+								</a>
+								<a href="#" className="text-xs lg:text-base">
+									Beauty Feed
+								</a>
+								<a href="#" className="text-xs lg:text-base">
+									Produk Organik
+								</a>
+							</div>
+						</div>
+						<div className="flex  flex-col gap-4 ">
+							<p className="text-left text-base font-bold sm:text-left sm:text-sm lg:text-lg">
+								PEMBAYARAN
+							</p>
+							<div className="flex flex-col place-items-start gap-2  sm:flex-row sm:flex-wrap lg:gap-4 ">
+								<img
+									src={bca}
+									className="w-[50%] object-contain sm:w-[60%] lg:w-[40%]"
+								/>
+								<img
+									src={bni}
+									className="w-[50%] object-contain sm:w-[60%] lg:w-[40%]"
+								/>
+								<img
+									src={permata}
+									className="w-[50%] object-contain sm:w-[60%] lg:w-[40%]"
+								/>
+								<img
+									src={bri}
+									className="w-[50%] object-contain sm:w-[60%] lg:w-[40%]"
+								/>
+							</div>
+						</div>
+						<div className="flex  flex-col gap-4 sm:col-start-2 ">
+							<p className=" text-right text-base font-bold sm:text-left sm:text-sm lg:text-lg">
+								PENGIRIMAN
+							</p>
+							<div className="flex flex-col place-items-end gap-2 sm:flex-row  sm:flex-wrap sm:place-items-start lg:gap-4">
+								<img
+									src={jne}
+									className="w-[50%] object-contain sm:w-[50%] lg:w-[30%]"
+								></img>
+								<img
+									src={pos}
+									className="w-[50%] object-contain sm:w-[50%] lg:w-[30%]"
+								></img>
+								<img
+									src={tiki}
+									className="w-[50%] object-contain sm:w-[50%] lg:w-[30%]"
+								></img>
+							</div>
+						</div>
 
-                </div>
-                <div className="footer__pembayaran ">
-                    <h3 className="text-sm mb-2">PEMBAYARAN</h3>
-                    <img src={BCA} className="py-1"></img>
-                    <img src={mandiri} className="py-1"></img>
-                    <img src={bni} className="py-1"></img>
-                </div>
-                <div className="footer__pengiriman ">
-                    <h3 className="text-sm mb-2">PENGIRIMAN</h3>
-                    <img src={jne} className="py-1"></img>
-                    <img src={pos} className="py-1"></img>
-                </div>
-                <div className="footer__customer ">
-                    <h3 className="text-sm mb-2">CUSTOMER CARE</h3>
-                    <a className='block'>WA</a>
-                    <a className='block'>Email</a>
-                    <a className='block'>Instagram</a>
-                    <a className='block'>No. Telepon</a>
-                </div>
-            </div>
-        </section>
-    )
+						<div className="flex  flex-col gap-4 sm:col-start-3 sm:row-span-2 sm:row-start-1">
+							<p className=" text-left text-base font-bold sm:text-left sm:text-sm lg:text-lg">
+								CUSTOMER CARE
+							</p>
+							<div className=" flex  flex-col place-items-start justify-between gap-2">
+								<div className="flex items-center gap-2">
+									<TbMail size={25} />
+									<div className="flex flex-col text-xs lg:text-sm">
+										<p className="font-semibold text-brown">Email</p>
+										<p>cs@flambo.com</p>
+									</div>
+								</div>
+								<div className="flex items-center gap-2">
+									<TbPhone size={25} />
+									<div className="flex flex-col text-xs lg:text-sm">
+										<p className="font-semibold text-brown">Telepon</p>
+										<p> (0)35 2568</p>
+									</div>
+								</div>
+								<div className="flex items-center gap-2">
+									<TbBrandWhatsapp size={25} />
+									<div className="flex flex-col text-xs lg:text-sm">
+										<p className="font-semibold text-brown">Whatsapp</p>
+										<p>+6282 3355 3778 </p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="h-[330px] sm:h-[400px] sm:w-2/5">
+						<div
+							className="flex h-full w-full items-center justify-center"
+							style={{
+								backgroundImage: `url(${illst})`,
+								backgroundPosition: "center",
+								backgroundSize: "cover",
+								backgroundRepeat: "no-repeat",
+							}}
+						>
+							<img src={google} alt="google" className="w-[70%]" />
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="font- bg-black py-6 text-center text-white lg:text-xl">
+				<p>© Flambo. All rights reserved.</p>
+			</section>
+		</>
+	);
 }
