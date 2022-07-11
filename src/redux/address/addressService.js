4c1f31a857df9419566bee36bdcd98249b6a1bbc
exa-extensia <exa.extensia@gmail.com>
2022-07-11 14:42:55 +0700

    [xa] pusing

--- src/redux/address/addressService.js
+++ src/redux/address/addressService.js
@@ -25,7 +25,7 @@ const CreateUserAddresss = async (
 	);
 	console.log(">>>> di service", response.data.data);
 
-	localStorage.setItem("address", JSON.stringify(response.data.data)); // bentuknya object {id: xx, user:{}, phone: x, receiver: xxxx, }
+	localStorage.setItem("address", JSON.stringify(response.data.data)); // bentuknya object {id: xx, user:{}, phone: x, receiver: xxx, }
 	// localStorage.setItem("address", JSON.stringify([response.data.data]));
 
 	// localStorage["address"] = JSON.stringify(response.data.data);
